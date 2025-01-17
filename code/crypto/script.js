3b7c9310f46651609d364895480442dd17683793
unknown <beda.brunner@mgb.ch>
2025-01-17 16:57:18 +0100

    fetch3

--- code/crypto/script.js
+++ code/crypto/script.js
@@ -1,15 +1,20 @@
-import { Cryptofetch } from './Cryptofetch.js';
-const dropdown = document.getElementById("dropdown-menu");
+import { CryptofetchHistorical } from './fetchhistoricalPrices.js';
+import { CryptofetchKline } from './fetchLatestKline.js';
+/*const dropdown = document.getElementById("dropdown-menu");
 const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("focus", () => {
   dropdown.classList.add("show");
-});
+});*/
 
-Cryptofetch("SOL", "1m"); //function for fetching historical Prices and latest kline
+let historicalPrices = CryptofetchHistorical("SOL", "1m"); //function for fetching historical Prices and latest kline
+let latestKline = CryptofetchKline("SOL", "1m");
 
+console.log(historicalPrices);
+console.log(latestKline);
+/*
 searchInput.addEventListener("blur", () => {
   setTimeout(() => {
     dropdown.classList.remove("show");
   }, 200);
-});
+});*/
