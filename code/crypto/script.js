5831ec67f619af9e57e5b427827113fb37bf623f
unknown <beda.brunner@mgb.ch>
2025-01-17 16:19:45 +0100

    fetch2

--- code/crypto/script.js
+++ code/crypto/script.js
@@ -1,9 +1,12 @@
+import { Cryptofetch } from './Cryptofetch.js';
 const dropdown = document.getElementById("dropdown-menu");
 const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("focus", () => {
   dropdown.classList.add("show");
 });
+
+Cryptofetch("SOL", "1m"); //function for fetching historical Prices and latest kline
 
 searchInput.addEventListener("blur", () => {
   setTimeout(() => {
